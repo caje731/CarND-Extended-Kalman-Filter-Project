f167bb0fc8054b9ba60cfe7ab34934056f19c0b9
Cajetan Rodrigues <caje731@gmail.com>
2017-09-10 20:49:07 +0530

    Add the missing logic for the Extended Kalman filter to work

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,6 +3,7 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
+
 KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
@@ -22,6 +23,10 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,11 +34,49 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+
+  //update the state by using Extended Kalman Filter equations
+  float x = x_[0];
+  float y = x_[1];
+  float ux = x_[2];
+  float uy = x_[3];
+
+  float rho = sqrt(x*x + y*y);
+  float theta = atan2(y,x);
+  float rhodot = (x*ux + y*uy)/rho;
+
+  VectorXd z_pred = VectorXd::Zero(3);
+  z_pred << rho, theta, rhodot;
+
+  VectorXd Y = z - z_pred;
+  //angle normalisation
+  Y[1] = atan2(sin(Y[1]),cos(Y[1]));
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * Y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
